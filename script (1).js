05ccb416255f04e9a740d0f4dc895a529cc2c536
Pâmela Brasil Gonella <131402720+Pamela-Brasil@users.noreply.github.com>
2025-03-24 21:28:31 -0300

    Add files via upload

---
+++ script (1).js
@@ -0,0 +1,38 @@
+
+
+/* 
+Desenvolver uma calculadora que recebe notas de alunos e calcula a média, 
+informando se o aluno foi aprovado ou reprovado. 
+*/
+
+/* Usar array para armazenar as notas. */
+
+let nomeDoAluno = prompt("Digite o nome do aluno: ")
+let notas = []
+let soma = notas.reduce((diariodenotas, nota) => diariodenotas + nota, 0)
+let media = soma / notas.length;
+
+
+/* Estruturas de repetição para permitir a entrada de várias notas. */
+
+    while (true) {
+        let nota = prompt("Digite a nota do aluno: ")
+        if (nota == null) {
+            break
+        }
+        notas.push(Number(nota))
+    }
+
+
+
+
+/* Estruturas de decisão para determinar a aprovação (média >= 7). 
+Exibir a média e o status (aprovado/reprovado)
+*/
+
+
+if (media >= 6){
+    document.getElementById('Resultado').innerHTML = "Parabéns você foi aprovado! Sua média é:", media
+}else{
+    document.getElementById('Resultado').innerHTML = "Desculpe você foi reprovado! Sua média é:", media
+    }
